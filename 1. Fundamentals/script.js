16a4ed7fe26c90c9ebdb81a389785e94f4d3cf10
YASH-GARG-1 <53114057+YASH-GARG-1@users.noreply.github.com>
2025-05-11 13:52:01 +0100

    Day-1: DataTypes

---
+++ 1. Fundamentals/script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+
+// if (js === "amazing") alert("Js is Fun!!");
+
+console.log(40+56+69+89);
+console.log('Jonas');
+
+let javascrptIsFun = true;
+console.log(javascrptIsFun);
+console.log(typeof javascrptIsFun);
+javascrptIsFun = 'I don\'t know'; //This is not good as we are converting the datatype of the variable.
+console.log(javascrptIsFun);
+console.log(typeof javascrptIsFun);
