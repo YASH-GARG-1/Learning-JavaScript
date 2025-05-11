d5073b84ed25571e1dd1da8d40d4efa4713424e3
YASH-GARG-1 <53114057+YASH-GARG-1@users.noreply.github.com>
2025-05-11 14:03:32 +0100

    Day-1: Let, const and var

--- 1. Fundamentals/script.js
+++ 1. Fundamentals/script.js
@@ -11,3 +11,19 @@ console.log(typeof javascrptIsFun);
 javascrptIsFun = 'I don\'t know'; //This is not good as we are converting the datatype of the variable.
 console.log(javascrptIsFun);
 console.log(typeof javascrptIsFun);
+
+// Const
+
+const myBirthYear = 2001;
+
+// var - Not recommended
+
+var job = 'programmer';
+console.log(job);
+
+var job = 'teacher'; //We were able to declare the job variable again.
+
+//Var is function Scoped and let is block scoped.
+
+//Never write the variable without declaring it as It has a global scope and is not recommended.
+
